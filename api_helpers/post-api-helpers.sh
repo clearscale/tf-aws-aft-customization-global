f6d22402826fa6ba05deb0e6e777a41398bb5db2
Joe Skomurski <joseph.skomurski@clearscale.com>
2024-01-27 10:22:26 -0500

    Update path

--- api_helpers/post-api-helpers.sh
+++ api_helpers/post-api-helpers.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
+echo "Executing Post-API Helpers"
 
 # echo "aws sts get-caller-identity"
 # aws sts get-caller-identity
-
 # aws ec2 describe-regions
+
+echo "----- Access folder: CD \API_Helpers -------"
+cd $DEFAULT_PATH/$CUSTOMIZATION/api_helpers/
 
 python3 ./delete_vpc.py
