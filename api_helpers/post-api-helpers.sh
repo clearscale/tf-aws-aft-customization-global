b5973eadfb70cd553c48b9df5afdcf851143a761
joseph-skomurski-clearscale <108933992+joseph-skomurski-clearscale@users.noreply.github.com>
2024-02-01 08:03:02 -0500

    Update delete-vpc script (#1)
    * Update delete-vpc script

--- api_helpers/post-api-helpers.sh
+++ api_helpers/post-api-helpers.sh
@@ -2,11 +2,9 @@
 
 echo "Executing Post-API Helpers"
 
-# echo "aws sts get-caller-identity"
-# aws sts get-caller-identity
-# aws ec2 describe-regions
-
-echo "----- Access folder: CD \API_Helpers -------"
 cd $DEFAULT_PATH/$CUSTOMIZATION/api_helpers/
 
-python3 ./delete_vpc.py
+# Must define region if running manually
+# export AWS_DEFAULT_REGION=us-west-2
+
+python3 ./delete_vpc.py '172.31.0.0/16'
